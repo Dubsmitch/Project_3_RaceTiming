febd313fec162d7fe9fa89f97baab75dfe35fb21
wsmitche <wsmitche@ncsu.edu>
2018-11-21 13:52:07 -0800

    created the two helper io classes

--- Project3/src/edu/ncsu/csc216/wolf_results/model/io/WolfResultsReader.java
+++ Project3/src/edu/ncsu/csc216/wolf_results/model/io/WolfResultsReader.java
@@ -1,5 +1,30 @@
 package edu.ncsu.csc216.wolf_results.model.io;
 
-public class WolfResultsReader {
+import edu.ncsu.csc216.wolf_results.race_results.RaceList;
 
+/**
+ * reads a race file from a specificed filename
+ * 
+ * @author William
+ *
+ */
+public class WolfResultsReader {
+	/**
+	 * creates a Wolf results Reader 
+	 * that can read files and create a raceList
+	 * from them
+	 */
+	public WolfResultsReader() {
+		
+	}
+	/**
+	 * reads a file and returns a racelist
+	 * @param filename
+	 * 		the file name to be read
+	 * @return RaceList
+	 * 		the races and results from the file 
+	 */
+	public RaceList readRaceListFile(String filename) {
+		return null;
+	}
 }
--- Project3/src/edu/ncsu/csc216/wolf_results/model/io/WolfResultsWriter.java
+++ Project3/src/edu/ncsu/csc216/wolf_results/model/io/WolfResultsWriter.java
@@ -1,5 +1,27 @@
 package edu.ncsu.csc216.wolf_results.model.io;
 
+import edu.ncsu.csc216.wolf_results.race_results.RaceList;
+/**
+ * A helper class that saves the race results to a file
+ * 
+ * @author William
+ *
+ */
 public class WolfResultsWriter {
-
+	/**
+	 * saves a race list file
+	 */
+	public WolfResultsWriter() {
+		
+	}
+	/**
+	 * writes a face file with a given file name and list
+	 * @param filename
+	 * 		the file name to save to
+	 * @param list
+	 * 		the list to be saved
+	 */
+	public static void writeRaceFile(String filename, RaceList list) {
+		//make the writer
+	}
 }
